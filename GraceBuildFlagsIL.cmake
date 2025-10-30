fd06326249a90ad74474ecfd198a5b0c1b38f211
memurro000 <telenkovvsevolod@gmail.com>
2025-10-30 18:12:30 +0300

    refactor: reorganize the CMake build flag system
    - move the flags declaration into an INTERFACE CMake library target
    - link the target to the library target to specify flag usage
    - export this dependency with GraceTargets
    - link the target to the tests and apps targets to specify flag usage
    - make the flags more portable by making sanitizer ones optional
    - comment out the flags not working as expected
    - mark the commented flags a subject to reconsider
    - remove exceeding CMake messages and obsolete commented lines
    - move build type message into the general CMakeLists
    - message in CMake that the MSVC-specific flags are not yet tested
    - partially styled the CMake code
    - update version patch number

---
+++ GraceBuildFlagsIL.cmake
@@ -0,0 +1,150 @@
+
+# TODO reconsider file name, extension and location
+
+add_library(Grace_build_flags INTERFACE)
+
+set_target_properties(Grace_build_flags PROPERTIES EXPORT_PROPERTIES "EXCLUDE_FROM_ALL")
+
+message(STATUS "Grace: " "MSVC build options are subject to test")
+message(STATUS "Grace: " "MSVC sanitizer build options are disabled now and are subject to change")
+
+target_compile_options(
+    Grace_build_flags
+    INTERFACE
+        # GNU, Clang, AppleClang
+        $<$<CXX_COMPILER_ID:GNU,Clang,AppleCLang>:
+            # Common
+                # Warnings
+                -Wall
+                -Wextra
+                -Wpedantic
+                -Wshadow
+                -Wnon-virtual-dtor
+                # -Wnull-dereference # TODO reconsider 
+                # -Wdouble-promotion # TODO reconsider
+
+                # Version-specific warnings
+                # $<$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,8>:-Wduplicated-cond>     # TODO reconsider 
+                # $<$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,6>:-Wduplicated-branches> # TODO reconsider 
+
+            # Debug
+            $<$<CONFIG:Debug>:
+                -O0
+                -g3
+#                -D_GLIBCXX_DEBUG # TODO reconsider (doesn't work with GTest)
+                -D_GLIBCXX_ASSERTIONS
+                -fno-inline
+                -fno-omit-frame-pointer
+                -fstack-protector-strong
+#                -fstack-clash-protection # TODO reconsider (reported to be unused)
+
+                # Sanitizer
+                $<$<BOOL:${ENABLE_CXX_OPTIONS_SAN}>:
+                    -fno-sanitize-recover=undefined
+                    -fsanitize-address-use-after-scope
+                    -fsanitize=address,undefined
+                    $<$<NOT:$<PLATFORM_ID:Windows,Darwin>>:
+                        -fsanitize=leak
+                    >
+                >
+                $<$<BOOL:${ENABLE_CXX_OPTIONS_NOLIB_UB_SAN}>:
+                    -fsanitize=undefined
+                    -fsanitize-undefined-trap-on-error
+                >
+            >
+
+            # Release
+            $<$<CONFIG:Release>:
+                -O3
+                -DNDEBUG
+                
+                # Performance
+                -funroll-loops
+                -fomit-frame-pointer
+#                -flto # TODO reconsider (file not recognized: file format not recognized)
+                -march=native
+                -mtune=native
+                
+                # TODO benchmark
+                # Security
+                -D_FORTIFY_SOURCE=2
+                -fstack-protector-strong
+            >
+
+            # RelWithDebInfo
+            $<$<CONFIG:RelWithDebInfo>:
+                -O2
+                -g
+                -DNDEBUG
+                -funroll-loops
+                -fomit-frame-pointer
+#                -flto # TODO reconsider (file not recognized: file format not recognized)
+            >
+
+            # MinSizeRel
+            $<$<CONFIG:MinSizeRel>:
+                -Os
+                -DNDEBUG
+                -fomit-frame-pointer
+#                -flto # TODO reconsider (file not recognized: file format not recognized)
+            >
+
+        >
+
+        # MSVC
+        $<$<CXX_COMPILER_ID:MSVC>:
+            # Common
+            /W4
+            /permissive-
+
+            # Debug
+            $<$<CONFIG:Debug>:
+                /Od
+                /Zi
+                /RTC1
+                /MDd
+            >
+
+            # Release
+            $<$<CONFIG:Release>:
+                /O2
+                /Ob2
+                /Gy
+                /DNDEBUG
+                /GL
+                /MD
+            >
+
+            # RelWithDebInfo
+            $<$<CONFIG:RelWithDebInfo>:
+                /O2
+                /Ob1
+                /Zi
+                /DNDEBUG
+                /MD
+            >
+
+        >
+
+)
+
+target_link_options(
+    Grace_build_flags
+    INTERFACE
+        $<$<CXX_COMPILER_ID:GNU,Clang,AppleClang>:
+            $<$<CONFIG:Debug>:
+                $<$<BOOL:${ENABLE_CXX_SAN_OPTIONS}>:
+                    -fno-sanitize-recover=undefined
+                    -fsanitize-address-use-after-scope
+                    -fsanitize=address,undefined
+                    $<$<NOT:$<PLATFORM_ID:Windows,Darwin>>:
+                        -fsanitize=leak
+                    >
+                >
+                $<$<BOOL:${ENABLE_CXX_NOLIB_UB_SAN_OPTION}>:
+                    -fsanitize=undefined
+                    -fsanitize-undefined-trap-on-error
+                >
+            >
+        >
+)
